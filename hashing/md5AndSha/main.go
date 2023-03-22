4d956b5ae3a826021546fb0e49ae2069a7b55d14
丹丹 <asd5405789789@gmail.com>
2023-03-22 12:18:28 +0800

    [ADD] 補上密碼檢查雜湊的方法學習

--- hashing/md5AndSha/main.go
+++ hashing/md5AndSha/main.go
@@ -5,7 +5,9 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"fmt"
+	"os"
 
+	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/crypto/sha3"
 )
 
@@ -29,4 +31,25 @@ func main() {
 	fmt.Println("SHA256   :", getHash("Hello World!", "SHA256"))
 	fmt.Println("SHA512   :", getHash("Hello World!", "SHA512"))
 	fmt.Println("SHA3_512 :", getHash("Hello World!", "SHA3_512"))
+
+	fmt.Println("以下開始是密碼檢查方法學習")
+	password := "mysecretpassword"
+	fmt.Println("密碼的明碼 :", password)
+
+	//用bcrypt 將密碼轉成雜湊值
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("密碼雜湊直:", string(hash))
+
+	//測試密碼是否一致
+	testString := "mysecretpassword"
+	err = bcrypt.CompareHashAndPassword([]byte(hash), []byte(testString))
+	if err != nil {
+		fmt.Println("密碼不符")
+	} else {
+		fmt.Println("密碼相符")
+	}
 }
